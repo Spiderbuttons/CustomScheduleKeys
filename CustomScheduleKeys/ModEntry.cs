b26a9172964e0a84cf97ea681d6b2df9143d2cb9
Spiderbuttons <azurylldmm@gmail.com>
2025-01-27 04:46:27 -0500

    Initial commit.

---
+++ CustomScheduleKeys/ModEntry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using CustomScheduleKeys.Helpers;
+
+namespace CustomScheduleKeys
+{
+    internal sealed class ModEntry : Mod
+    {
+        internal static IModHelper ModHelper { get; set; } = null!;
+        internal static IMonitor ModMonitor { get; set; } = null!;
+        internal static Harmony Harmony { get; set; } = null!;
+
+        public override void Entry(IModHelper helper)
+        {
+            ModHelper = helper;
+            ModMonitor = Monitor;
+            Harmony = new Harmony(ModManifest.UniqueID);
+
+            Harmony.PatchAll();
+
+            Helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        }
+
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+
+            if (e.Button is SButton.F2)
+            {
+                
+            }
+        }
+    }
+}
---
+++ CustomScheduleKeys/ScheduleData.cs
@@ -0,0 +1,6 @@
+﻿namespace CustomScheduleKeys;
+
+public class ScheduleData
+{
+    
+}
